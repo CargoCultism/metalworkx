72906f688b012657e4fa1f508b17ab30ca368090
CargoCultism <lutz@rothenserv.de>
2023-09-24 20:59:14 +0200

    Create tkdemo.py

---
+++ src/tkdemo.py
@@ -0,0 +1,89 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+import json
+from json import JSONEncoder
+from dataclasses import dataclass
+
+@dataclass
+class InventoryItem:
+	name: str
+	unit_price: float
+	quantity_on_hand: int = 0
+
+	def total_cost(self) -> float:
+		return self.unit_price * self.quantity_on_hand
+
+class InventoryItemEnconder(JSONEncoder):
+	def default(self,o):
+		return o.__dict__
+
+version = "0.1a"
+
+window = tk.Tk()
+window.title(version)
+window.resizable(0, 0)
+
+def loadCallBack():
+	if not filename:
+		msg = messagebox.showinfo("Hello Python", "Filename is empty")
+	else:
+		f = open("asdfsaDF", "r")
+		content = f.read()
+		f.close()
+		
+		parsed = json.loads(content)
+		ii = InventoryItem(**parsed)
+		print(ii.name)
+
+def saveCallBack(fname, item):
+	filename = fname.get()
+	if not filename:
+		msg = messagebox.showinfo("Hello Python", "Filename is empty")
+	else:
+		f = open(filename, "a")
+		f.write(InventoryItemEnconder().encode(item))
+		f.close()
+
+
+window.columnconfigure(0,weight=4)
+window.columnconfigure(1,weight=1)
+
+firstitem = InventoryItem(name = 'asdf', quantity_on_hand = 7, unit_price = 50.0)
+
+leftframe = ttk.Frame(window)
+leftframe.columnconfigure(0,weight=0)
+leftframe.grid(column=0,row=0)
+
+ttk.Label(leftframe, text='Filename:').grid(column=0,row=0)
+filename = ttk.Entry(leftframe, width='30')
+filename.grid(column=1,row=0)
+
+ttk.Label(leftframe, text='Product Name:').grid(column=0,row=1)
+productName = ttk.Entry(leftframe, width='30', textvariable=firstitem.name)
+productName.insert(0, firstitem.name)
+productName.grid(column=1,row=1)
+
+ttk.Label(leftframe, text='Product Quantity:').grid(column=0,row=2)
+productQty = ttk.Entry(leftframe, width='30')
+productQty.insert(0, firstitem.quantity_on_hand)
+productQty.grid(column=1,row=2)
+
+ttk.Label(leftframe, text='Unit Price:').grid(column=0,row=3)
+unitPrice = ttk.Entry(leftframe, width='30')
+unitPrice.insert(0, firstitem.unit_price)
+unitPrice.grid(column=1,row=3)
+
+topframe = ttk.Frame(window)
+topframe.columnconfigure(0,weight=1)
+topframe.grid(column=1,row=0)
+
+#bottomframe = tk.Frame(window)
+#bottomframe.pack(side = tk.BOTTOM)
+
+ttk.Button(topframe, text = "Load", command = loadCallBack).grid(column=0,row=0)
+ttk.Button(topframe, text = "Save", command = lambda j=firstitem: saveCallBack(filename,firstitem)).grid(column=0,row=1)
+
+#buttonExit = tk.Button(bottomframe, text = "Exit") 
+
+window.mainloop()
